f01d49c20e5fdf3d9d118d5a42c5f5510c4106e5
Vladimir Bogretsov <v.bogretsov@gmail.com>
2013-06-24 00:46:49 +0400

    #4 : Corrected navigation

--- src/Academy/Academy.Presentation.Views/Scripts/AnonymousNavigation.js
+++ src/Academy/Academy.Presentation.Views/Scripts/AnonymousNavigation.js
@@ -1,1 +1,41 @@
-﻿
+﻿$(function () {
+
+    var bodyId = "#body";
+
+    var app = $.sammy('#body', function (context) {
+
+        this.get('#/Contacts', function () {
+            alert('contacts');
+            LoadView(bodyId, "Home/Contacts");
+        });
+
+        this.get('#/About', function () {
+            alert('about');
+            LoadView(bodyId, "Home/About");
+        });
+
+        this.get('#/SearchArticles', function () {
+            LoadView(bodyId, "Search/SearchArticles");
+        });
+
+        this.get('#/SearchQuestions', function () {
+            LoadView(bodyId, "Search/SearchQuestions");
+        });
+
+        return false;
+    });
+
+    $(function () {
+        app.run('#/');
+    });
+});
+
+function LoadView(divId, request, complete) {
+    $.ajax({
+        url: request,
+        cache: false,
+        success: function (result) {
+            $(divId).html(result);
+        }
+    });
+}
